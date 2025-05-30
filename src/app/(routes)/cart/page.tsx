f797755f03a5ac57d12bc7783c53fd753a5e12fa
mohammad rida <mhmd_rida_990@hotmail.com>
2025-05-30 20:26:47 +0300

    add loading pages

--- src/app/(routes)/cart/page.tsx
+++ src/app/(routes)/cart/page.tsx
@@ -5,22 +5,27 @@ import useCart from "@/hooks/use-cart";
 import React, { useEffect, useState } from "react";
 import CartItem from "./components/CartItem";
 import Summery from "./components/Summery";
+import SpinnerLoading from "@/components/Spinner";
 
 const CartPage = () => {
   const [isMounted, setIsMounted] = useState(false);
+  const cart = useCart();
+
+  // You can adjust this condition based on your actual loading logic
+  const isLoading = !isMounted || !cart;
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
-  const cart = useCart();
 
-  if (!isMounted) {
-    return null;
+  if (isLoading) {
+    return <SpinnerLoading />;
   }
+
   return (
     <div className="bg-white">
       <Container>
-        <div className="px-4 4 py-16 sm:px-6 lg:px-8">
+        <div className="px-4 py-16 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
 
           <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+import { LoaderCircle } from "lucide-react";
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="text-4xl bg-gray-100 rounded-2xl w-screen h-screen flex justify-center items-center my-auto">
+      <LoaderCircle size={60} className="text-sky-500 animate-spin " />
+    </div>
+  );
+};
+
+export default Loading;
