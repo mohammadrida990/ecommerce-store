e94ad06a1f98ec39011038ffd5b8fe69d0cf6297
mohammad rida <mhmd_rida_990@hotmail.com>
2025-05-31 19:16:50 +0300

    add banner for redirect to admin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import ModalProvider from "@/providers/modal-provider";
 import ToastProvider from "@/providers/toast-provider";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -39,6 +40,20 @@ export default function RootLayout({
         <ModalProvider />
 
         <ToastProvider />
+
+        <div className="flex items-center justify-center bg-sky-200 w-full space-x-2 h-10">
+          <span className="text-sm md:text-lg text-gray-500">
+            Control Store in the CMS:
+          </span>
+
+          <Link
+            href="https://ecommerce-admin-six-green.vercel.app/cmb6fpw8200005h3krap4djih"
+            target="_blink"
+            className="underline text-green-700 hover:text-green-400 text-sm md:text-lg"
+          >
+            E-Commerce Admin
+          </Link>
+        </div>
 
         <Navbar />
 
